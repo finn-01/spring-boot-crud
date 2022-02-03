f47f8c2ad1cac9ff79eb04e1c3fd37bf5ae30825
finn-01 <nguyensam576@gmail.com>
2022-02-03 15:32:28 +0700

    create project

---
+++ src/main/java/com/example/crudspringreactjs/CrudSpringReactjsApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudspringreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringReactjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringReactjsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudspringreactjs/CrudSpringReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudspringreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringReactjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
