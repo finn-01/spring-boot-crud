3139011a8d0d8374877b832d379c1e1f8816a0f4
finn-01 <nguyensam576@gmail.com>
2022-02-04 12:19:14 +0700

    model -employee.java

---
+++ src/main/java/com/example/crudspringreactjs/model/Employee.java
@@ -0,0 +1,29 @@
+package com.example.crudspringreactjs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "fist_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+}
