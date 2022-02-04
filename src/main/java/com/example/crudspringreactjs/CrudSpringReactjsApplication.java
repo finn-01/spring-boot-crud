1b5cd3ca651c7958f983cbc3817a076780bec3fe
finn-01 <nguyensam576@gmail.com>
2022-02-04 14:07:22 +0700

    get api in postman

--- src/main/java/com/example/crudspringreactjs/CrudSpringReactjsApplication.java
+++ src/main/java/com/example/crudspringreactjs/CrudSpringReactjsApplication.java
@@ -1,13 +1,34 @@
 package com.example.crudspringreactjs;
 
+import com.example.crudspringreactjs.model.Employee;
+import com.example.crudspringreactjs.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudSpringReactjsApplication {
+public class CrudSpringReactjsApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CrudSpringReactjsApplication.class, args);
     }
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Employee employee = new Employee();
+        employee.setFirstName("Finn");
+        employee.setLastName("Nguyen");
+        employee.setEmailId("finnnguyen@gmail.com");
+        employeeRepository.save(employee);
+
+        Employee employee1 = new Employee();
+        employee1.setFirstName("Finn1");
+        employee1.setLastName("Nguyen1");
+        employee1.setEmailId("finnnguyen1@gmail.com");
+        employeeRepository.save(employee1);
+    }
 }
---
+++ src/main/java/com/example/crudspringreactjs/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.crudspringreactjs.controller;
+
+import com.example.crudspringreactjs.model.Employee;
+import com.example.crudspringreactjs.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/employees")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+}
