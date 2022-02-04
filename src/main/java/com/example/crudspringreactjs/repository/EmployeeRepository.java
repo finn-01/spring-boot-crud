73a144e5af05e8551b0e28ca66c380cefb3abbe2
finn-01 <nguyensam576@gmail.com>
2022-02-04 12:22:28 +0700

    repository-employeerepository.java

---
+++ src/main/java/com/example/crudspringreactjs/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.crudspringreactjs.repository;
+
+import com.example.crudspringreactjs.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    //all crud database methods
+}
