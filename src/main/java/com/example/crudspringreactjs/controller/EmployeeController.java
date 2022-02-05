4e8399ed56a6792f4100da34defe376ff612993f
finn-01 <nguyensam576@gmail.com>
2022-02-05 11:56:08 +0700

    Build Update Employee REST API

--- src/main/java/com/example/crudspringreactjs/controller/EmployeeController.java
+++ src/main/java/com/example/crudspringreactjs/controller/EmployeeController.java
@@ -35,4 +35,19 @@ public class EmployeeController {
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
         return ResponseEntity.ok(employee);
     }
+
+    //build update employee API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employeeDetail){
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        updateEmployee.setFirstName(employeeDetail.getFirstName());
+        updateEmployee.setLastName(employeeDetail.getLastName());
+        updateEmployee.setEmailId(employeeDetail.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+
+        return ResponseEntity.ok(updateEmployee);
+    }
 }
