b043038fc54601e347342734e4c58036cc494a99
finn-01 <nguyensam576@gmail.com>
2022-02-05 11:47:32 +0700

    Build Get Employee By Id REST API

--- src/main/java/com/example/crudspringreactjs/controller/EmployeeController.java
+++ src/main/java/com/example/crudspringreactjs/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.crudspringreactjs.controller;
 
+import com.example.crudspringreactjs.exception.ResourceNotFoundException;
 import com.example.crudspringreactjs.model.Employee;
 import com.example.crudspringreactjs.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,13 @@ public class EmployeeController {
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    //Get id
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok(employee);
     }
 }
